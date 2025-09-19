f1e6fa73b1b8b80c1702ea8b8f1469f0ffa119ab
CodeCraftingDevelopment <christophe.pauliac@codecrafting.fr>
2025-09-19 19:24:27 +0200

    Project Configuration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+const isStaging = process.env.NEXT_PUBLIC_ENV === 'staging';
+const isDevelopment = process.env.NEXT_PUBLIC_ENV === 'development';
+
 const nextConfig: NextConfig = {
     reactStrictMode: true,
     output: "standalone",  // 🔥 utile pour Docker
+    productionBrowserSourceMaps: isStaging,
+    typescript : {
+        tsconfigPath: isStaging || isDevelopment ? './tsconfig.json' : './tsconfig.prod.json',
+    }
 };
 
 export default nextConfig;
