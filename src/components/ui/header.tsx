21dfde99747a687a0fdf191d0cef338c3a883fc1
CodeCraftingDevelopment <christophe.pauliac@codecrafting.fr>
2025-10-28 20:15:56 +0100

    Responsive header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -6,40 +6,69 @@ const color = ""
 
 
 export default function Header(){
+
+    const gridItems: React.CSSProperties = {
+        backgroundColor: 'teal',
+        overflow: 'hidden'
+    };
+
+    const content: React.CSSProperties = {
+        backgroundColor: 'cadetblue',
+    };
+
     return (
         <header>
 
                 <Grid
                     templateColumns={"repeat(12, 1fr)"}
-                    templateRows={"repeat(3, 1fr)"}
+                    templateRows={"repeat(2, 1fr)"}
                     gap={2}
                     padding={2}
                     shadow="md"
                 >
-                    <GridItem bg={color} rowSpan={3} colSpan={1}>
-                        <Box pb={3} pr={3}>
+                    <GridItem bg={color}
+                              rowSpan={2}
+                              colSpan={{ base: 12, sm: 2, md: 2, lg: 2 }}
+                              // minW={{ base: "100%", md: "200px", lg: "120px" }}
+                              minW={120}
+                              style={gridItems}>
+                        <Box style={content}>
                             <CodecraftLogo width={120} height={120}/>
                         </Box>
                     </GridItem>
-                    <GridItem bg={color} colSpan={9} rowSpan={1} p={2}
+                    <GridItem bg={color}
+                              colSpan={{ base: 12, sm: 10, md: 8, lg: 8 }}
+                              rowSpan={1}
+                              p={2}
+                              style={gridItems}
                               display="flex"
                               alignItems="center"
                     >
                         <Flex gap="4">
-                            <p>Environnement : {process.env.NEXT_PUBLIC_ENV}</p>
-                            <a>test</a>
+                            <p style={content}>Environnement : {process.env.NEXT_PUBLIC_ENV}</p>
+                            <a style={content}>test</a>
                         </Flex>
                     </GridItem>
-                    <GridItem bg={color} colSpan={2} rowSpan={1} p={2}
+                    <GridItem bg={color}
+                              // colSpan={2}
+                              colSpan={{ base: 12, sm: 10, md: 2, lg: 2 }}
+                              rowSpan={1}
+                              p={2} style={gridItems}
                               display="flex"
-                              alignItems="center" justifyContent="flex-end"
+                              alignItems="center"
+                              justifyContent="flex-end"
                     >
-                        <ColorModeButton padding={0} margin={0} size={"2xs"}/>
+                        <ColorModeButton padding={0} margin={0} size={"2xs"} style={content}/>
 
                     </GridItem>
-                    <GridItem bg={color} colSpan={11} rowSpan={2} p={2}>
+                    <GridItem bg={color}
+                              colSpan={{ base: 12, sm: 12, md: 10, lg: 10 }}
+                              // colSpan={10}
+                              rowSpan={1}
+                              p={2}
+                              style={gridItems}>
 
-                        <p>dsfsd</p>
+                        <p style={content}>dsfsd</p>
                     </GridItem>
                 </Grid>
 
