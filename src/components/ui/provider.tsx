b579fd137f64c4f14572b5f894c2a9ea3a5377ec
CodeCraftingDevelopment <christophe.pauliac@codecrafting.fr>
2025-11-10 10:22:26 +0100

    lint

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -2,10 +2,7 @@
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
-import {
-  ColorModeProvider,
-  type ColorModeProviderProps,
-} from "./color-mode";
+import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
 
 export function Provider(props: ColorModeProviderProps) {
   const { children, ...rest } = props;
