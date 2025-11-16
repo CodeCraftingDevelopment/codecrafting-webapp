fb64e67371be2f4a4887dd7f6c931fcadee509e0
CodeCraftingDevelopment <christophe.pauliac@codecrafting.fr>
2025-11-16 12:40:00 +0100

    Suppression warnings

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-import { hashPassword } from "../src/lib/auth/password";
+import { hashPassword } from "@/lib/auth/password";
 
 const prisma = new PrismaClient();
 
 async function main() {
   // Créer un admin
   const adminPassword = await hashPassword("Passw0rd!");
-  const _admin = await prisma.user.upsert({
+  await prisma.user.upsert({
     where: { email: "alice@codecrafting.fr" },
     update: {},
     create: {
@@ -19,7 +19,7 @@ async function main() {
 
   // Créer un member
   const memberPassword = await hashPassword("Passw0rd!");
-  const _member = await prisma.user.upsert({
+  await prisma.user.upsert({
     where: { email: "bob@codecrafting.fr" },
     update: {},
     create: {
